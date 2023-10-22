54828f568f198b974faf9d2a650c150097daf8ce
Peter Humphreys <83676679+PeterHumphreys@users.noreply.github.com>
2023-10-22 10:09:05 -0500

    Spam control

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -1,28 +1,60 @@
 const form = document.querySelector("form#contact");
 const nameField = document.querySelector("#name-field");
+const name2Field = document.querySelector("#name-2-field");
 const emailField = document.querySelector("#email-field");
 const messageField = document.querySelector("#message-field");
 const submitBtn = document.querySelector("#submit-btn");
 
-form.addEventListener("submit", (event)=>
+form.addEventListener("submit", async function (event)
 {
   event.preventDefault();
-  console.log("In submit")
-  //If message is annoying bot crawl that fills up email inbox, prevent submission
-  //and infinitely loop
-  if (messageField.value.length > 0 && messageField.value.length < 10)
+  //If message is annoying bot crawl that fills up email inbox, prevent submission and infinitely loop
+  /*if (nameField.value || (messageField.value.length > 0 && messageField.value.length < 10) )
   {
     submitBtn.disabled = true;
     while (true)
     {
-      alert("Begone now and forevermore.")
+      alert("Spam detected!")
     }
-
+  }*/
+  if (false)
+  {
+    console.log("Are you in a parallel universe or something???")
   }
+
   //Otherwise, proceed as normal
   else
   {
-    form.submit();
+    const formData = new FormData();
+    formData.append(this[0].name, this[0].value); //Name
+    formData.append(this[1].name, this[1].value); //Name-2
+    formData.append(this[2].name, this[2].value); //Email
+    formData.append(this[3].name, this[3].value); //Message
+
+    //Attempt to POST JSON data to server
+    try
+    {
+        const response = await fetch('php/form_process.php',
+        {
+            method: 'POST',
+            body: formData
+        });
+        //submission successful, open main page
+        if (response.ok)
+        {
+            location.href=response.url;
+        }
+        //submission failed, display error message
+        else
+        {
+          alert("Something went wrong on my side, please try again")
+        }
+    }
+    //Server error occurred
+    catch(error)
+    {
+        console.log(error);
+    }
   }
 });
 
--- src/php/form_process.php
+++ src/php/form_process.php
@@ -1,24 +1,43 @@
 <?php
-	$name = $_POST['name'];
+	$pieceOfTrashSpammerName = $_POST['name'];
+	$name = $_POST['name_2'];
 	$visitor_email = $_POST['email'];
 	$message = $_POST['message'];
+	$isPeriodJerk = strlen($message) > 0 && strlen($message) <= 10;
 	
-	$email_from = 'noreply@phumphreys.com';
-	
-	$email_subject = "New Form Submission";
-	
-	$email_body = "User Name: $name.\n".
-					"User Email: $visitor_email.\n".
-						"User Message: $message.\n";
-						
-	$to = "the_phscale@yahoo.com";
-	
-	$headers = "From: $email_from \r\n";
-	
-	$headers .= "Reply-To: $visitor_email \r\n";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	
-	header("Location:../index.html");
-	
+	// If spammer filled out the honeypot field, send them a nice little thank you present
+	if (!empty($pieceOfTrashSpammerName) || $isPeriodJerk)
+	{
+		$headers = "From: AnnoyedPerson@StopSpammingMe.com \r\n";
+		$headers .= "Reply-To: $visitor_email \r\n";	
+		$to = $visitor_email;
+		$email_subject = "Spammers will not be tolerated";
+		
+		//If this is the obnoxious jerk that sends me 2 emails with 1 period in each input field everyday at 4:17 p.m.,
+		//write a more personal message
+		if ($isPeriodJerk)
+		{
+			$email_body = "Hey, you obnoxious jerk who loves sending me 2 freaking messages everyday at 4:17 P.M. with a \".\" in each input field.  I have a message for you-\n\n\n\".\"\n\n\nP.S. - If you try to keep this up, I swear that I will absolutely make it my life's mission to hunt you down and smash your computer and whatever stupid bot your pathetic personage is using to send this stupid email into absolute oblivion.  I am sick and tired of looking through your stupid messages all the time.\n\nI am a programmer.  I know what I'm doing. I know how to find you.  Don't tempt me.";
+		}
+		else 
+		{
+			$email_body = "Hi, $name, go spam someone else.  If you try to keep this up, I swear that I will absolutely make it my life's mission to hunt you down and smash your computer and whatever stupid bot your pathetic personage is using to send this stupid email into absolute oblivion.  I am sick and tired of looking through your stupid messages all the time.\n\nI am a programmer.  I know what I'm doing. I know how to find you.  Don't tempt me.";
+		}
+		mail($to,$email_subject,$email_body, $headers);
+		header("Location:https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+	}
+	// This is a legitimate message
+	else 
+	{
+		$email_from = 'noreply@phumphreys.com';
+		$email_subject = "New Form Submission";
+		$email_body = "User Name: $name.\n".
+						"User Email: $visitor_email.\n".
+							"User Message: $message.\n";
+		$to = "the_phscale@yahoo.com";
+		$headers = "From: $email_from \r\n";
+		$headers .= "Reply-To: $visitor_email \r\n";	
+		mail($to,$email_subject,$email_body,$headers);
+	}
+	header("Location:../thank-you.html");
 ?>
